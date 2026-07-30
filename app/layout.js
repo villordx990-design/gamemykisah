15227c5a14b0c38d474028fdc2a2af159f142b36
Vill <villordx990@gmail.com>
2026-07-30 14:30:25 +0700

    Upload via up2git

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,12 @@ export const metadata = {
   description: 'Situs download game hasil translate Bahasa Indonesia',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
